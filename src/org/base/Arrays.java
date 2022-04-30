76c5fb0d9ab951cae64fface1b13002dfbb68d0b
Mathan0302Kumar <madhan511kumar@gmail.com>
2022-04-30 14:34:38 +0530

    Array First Push

---
+++ src/org/base/Arrays.java
@@ -0,0 +1,34 @@
+package org.base;
+
+
+public class Arrays{
+	
+	public static void main(String[] args) {
+		int a[] = new int[15];
+		a[0]  = 1;
+		a[1]  = 2;
+		a[2]  = 3;
+		a[3]  = 4;
+		a[4]  = 5;
+		a[5]  = 6;
+		a[6]  = 4;
+		a[7]  = 3;
+		a[8]  = 7;
+		a[9]  = 2;
+		a[10] = 8;
+		a[11] = 9;
+		a[12] = 1;
+		a[13] = 7;
+		a[14] = 4;
+		
+		System.out.println(a[12]);
+		System.out.println("**********");
+		
+		int length = a.length;
+		System.out.println(length);
+		System.out.println("***********");
+		System.out.println("For loop");
+		
+		
+	}
+}
---
+++ src/org/base/Sample.java
@@ -0,0 +1,26 @@
+package org.base;
+
+public class Sample {
+	int a = 5;
+	int b = 188;
+	int c = a + b;
+
+	private void savings(int a, int b) {
+		int c = 0;
+		c = a - b;
+		System.out.println(c);
+		c = a + b;
+		System.out.println(c);
+
+	}
+
+	public static void main(String[] args) {
+
+		Sample mn = new Sample();
+		System.out.println(mn.c);
+		mn.savings(55, 89);
+		System.out.println(mn.c);
+
+	}
+
+}
